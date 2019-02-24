2ff3263c96991e2a409e13ac4514a7d9cfb9c1e0
Nemani, Arun <Arun.Nemani@usfoods.com>
2019-02-24 01:15:17 -0600

    v1 of package

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os.path as P
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open(P.join(P.dirname(__file__), 'requirements.txt')) as f:
+    requires = f.read().splitlines()
+
+setuptools.setup(
+    name="dfreduce",
+    version="1.0",
+    author="Arun Nemani",
+    author_email="neman014@gmail.com",
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    description="Automatic size reduction for pandas dataframe",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/arun-nemani/dfreduce",
+    packages=setuptools.find_packages(),
+    install_requires=requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
